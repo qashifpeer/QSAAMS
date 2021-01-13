9834f6df967a228fe7ee5085b95c2d4cc0e5de2c
qashifpeer <46715022+qashifpeer@users.noreply.github.com>
2021-01-13 10:18:40 +0530

    Edit Onclick Listener Tested

--- app/src/main/java/com/zeomawer/qsams/StudentDetails.java
+++ app/src/main/java/com/zeomawer/qsams/StudentDetails.java
@@ -3,14 +3,18 @@ package com.zeomawer.qsams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StudentDetails extends AppCompatActivity {
     EditText t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11;
 
     Button b1;
+    FloatingActionButton f1,f2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,21 @@ public class StudentDetails extends AppCompatActivity {
 
         initializeWidgets();
         initializeListeners();
+        fetchData();
+
+
+        f1.setOnClickListener(this::editData);
+        f2.setOnClickListener(this::updateData);
 
 
 
+    }
 
+    private void updateData(View view) {
+
+    }
+
+    private void fetchData() {
         t1.setText(getIntent().getStringExtra("name").toString());
         t2.setText(getIntent().getStringExtra("fathername").toString());
         t3.setText(getIntent().getStringExtra("mothername").toString());
@@ -35,6 +50,13 @@ public class StudentDetails extends AppCompatActivity {
         t10.setText(getIntent().getStringExtra("dob").toString());
         t11.setText(getIntent().getStringExtra("RollNumber").toString());
 
+    }
+
+    private void editData(View view) {
+        t1.setFocusable(true);
+        t1.setFocusableInTouchMode(true);
+        t1.setCursorVisible(true);
+        t1.setClickable(true);
 
     }
 
@@ -55,11 +77,8 @@ public class StudentDetails extends AppCompatActivity {
         t10=findViewById(R.id.txtDob);
         t11=findViewById(R.id.txtRollN);
 
-
-
-
-
-        b1=findViewById(R.id.btnBack);
+        f1=findViewById(R.id.faEdit);
+        f2=findViewById(R.id.faUpdate);
 
 
     }
